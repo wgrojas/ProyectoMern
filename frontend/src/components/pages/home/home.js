9a3b4c3c7545044482745a4a5bdaad98f62c85ee
wgrojas <wgrojas1205@gmail.com>
2021-12-01 21:22:57 -0500

    Ajustes al home y menu

--- frontend/src/components/pages/home/home.js
+++ frontend/src/components/pages/home/home.js
@@ -1,33 +1,23 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import './home.css';
+import React from "react";
+import {Carousel} from "react-bootstrap";
+import "./home.css";
 
 export default class Home extends React.Component {
     render() {
         return (
             <Carousel>
                 <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="assets\\7.png" width="20%" height="650"
-                        alt="Third slide"
-                    />
-
+                    <img className="d-block w-100" src="assets\\1.jpg" width="20%" height="900" alt="Firs slide"/>
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3>Computadores Gamer</h3>
+                        <p>Vive la experiencia gamer a su maxima potencia con equipos avanzados </p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="assets\\1.jpg" width="20%" height="650"
-                        alt="Third slide"
-                    />
-
+                    <img className="d-block w-100" src="assets\\7.png" width="20%" height="900" alt="Second slide"/>{" "}
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3>Todo lo que necesitas para tu computador</h3>
+                        <p>Todos los componentes que necesitas para armar tu Equipo de computo</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
